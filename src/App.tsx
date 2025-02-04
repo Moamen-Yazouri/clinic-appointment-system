74d0cb8e97609b8b32194391ef7284cd63373f39
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-02-04 12:48:03 +0200

    Add Chart and some styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import CreateAppointment from "./Screens/CreateAppointment/CreateAppointment";
-// import Dashboard from "./Screens/Dashboard/Dashboard";
+import Dashboard from "./Screens/Dashboard/Dashboard";
 import { IAppointment, Status } from "./types/types";
 // import ManageAppointments from "./Screens/ManageAppointments/ManageAppointments";
 function App() {
-  // const apps: IAppointment[] = JSON.parse(localStorage.getItem("appointment-details") || "[]");
+  const apps: IAppointment[] = JSON.parse(localStorage.getItem("appointment-details") || "[]");
   
   return (
     <>
-      <h1>Clinic Appointment System</h1>
-      {/* <Dashboard appointments={apps}/> */}
+      {/* <h1>Clinic Appointment System</h1> */}
+      <Dashboard appointments={apps}/>
       {/* <CreateAppointment/> */}
       {/* <ManageAppointments /> */}
     </>
