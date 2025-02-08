a1dfb8ef36e91b867090beb50fe9afdca9539d10
Qosay-Abu-Alrob <qusai-2011@hotmail.com>
2025-02-08 15:09:46 +0200

    Update: add a validation on name and age form and refactor Create Appointment

--- src/Screens/CreateAppointment/CreateAppointment.tsx
+++ src/Screens/CreateAppointment/CreateAppointment.tsx
@@ -2,7 +2,7 @@ import "./CreateAppointment.css";
 import { useState } from "react";
 import { IAppointment, Status } from "../../types/types";
 import dayjs from "dayjs";
-import DateTimePicker from "../../components/DateTimePicker/DateTimePicker";
+import CreateAppointmentForm from "../../components/CreateAppointmentForm/CreateAppointmentForm";
 
 const CreateAppointment = () => {
   const appointmentsNumber = JSON.parse(
@@ -86,58 +86,12 @@ const CreateAppointment = () => {
       ) : (
         <>
           <h2>Create Appointment</h2>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="name"
-              placeholder="Patient Name"
-              value={patientData.name}
-              onChange={handleChange}
-              required
-            />
-            <input
-              type="text"
-              name="contact"
-              placeholder="Contact"
-              value={patientData.contact}
-              onChange={handleChange}
-              required
-            />
-            <input
-              type="number"
-              name="age"
-              placeholder="Age"
-              value={patientData.age}
-              onChange={handleChange}
-              required
-            />
-            <select
-              className="gender"
-              name="gender"
-              value={patientData.gender}
-              onChange={handleChange}
-              required
-            >
-              <option value="">Select Gender</option>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-            </select>
-
-            <DateTimePicker
-              selectedDate={patientData.dateTime}
-              onDateChange={handleDateChange}
-            />
-
-            <textarea
-              name="symptoms"
-              placeholder="Describe Symptoms"
-              value={patientData.symptoms}
-              onChange={handleChange}
-              required
-            ></textarea>
-
-            <button type="submit">Submit</button>
-          </form>
+          <CreateAppointmentForm
+          patientData={patientData}
+          handleChange={handleChange}
+          handleDateChange={handleDateChange}
+          handleSubmit={handleSubmit}
+        />
         </>
       )}
     </div>
---
+++ src/components/CreateAppointmentForm/CreateAppointmentForm.tsx
@@ -0,0 +1,81 @@
+import DateTimePicker from "../../components/DateTimePicker/DateTimePicker";
+import { IAppointment } from "../../types/types";
+
+interface IAppointmentFormProps {
+  patientData: IAppointment;
+  handleChange: (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => void;
+  handleDateChange: (date: Date | null) => void;
+  handleSubmit: (e: React.FormEvent) => void;
+}
+
+const CreateAppointmentForm: React.FC<IAppointmentFormProps> = ({
+  patientData,
+  handleChange,
+  handleDateChange,
+  handleSubmit,
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        min={3}
+        max={25}
+        placeholder="Patient Name"
+        value={patientData.name}
+        onChange={handleChange}
+        required
+      />
+      <input
+        type="text"
+        name="contact"
+        placeholder="Contact"
+        value={patientData.contact}
+        onChange={handleChange}
+        required
+      />
+      <input
+        type="number"
+        name="age"
+        placeholder="Age"
+        min={18}
+        max={100}
+        value={patientData.age}
+        onChange={handleChange}
+        required
+      />
+      <select
+        className="gender"
+        name="gender"
+        value={patientData.gender}
+        onChange={handleChange}
+        required
+      >
+        <option value="">Select Gender</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+      </select>
+
+      <DateTimePicker
+        selectedDate={patientData.dateTime}
+        onDateChange={handleDateChange}
+      />
+
+      <textarea
+        name="symptoms"
+        placeholder="Describe Symptoms"
+        value={patientData.symptoms}
+        onChange={handleChange}
+        required
+      ></textarea>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default CreateAppointmentForm;
