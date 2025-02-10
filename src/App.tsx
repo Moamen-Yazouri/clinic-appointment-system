c42155e31ee162fd5c14878e395d13dd87105808
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2025-02-10 22:33:15 +0200

    Install react-router-dom and set paths for each screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import CreateAppointment from "./Screens/CreateAppointment/CreateAppointment";
-// import ManageAppointments from "./Screens/ManageAppointments/ManageAppointments";
-// import Dashboard from "./Screens/Dashboard/Dashboard";
-// import { IAppointment, Status } from "./types/types";
+import ManageAppointments from "./Screens/ManageAppointments/ManageAppointments";
+import Dashboard from "./Screens/Dashboard/Dashboard";
+import { IAppointment } from "./types/types";
+
 function App() {
-  //   const apps: IAppointment[] = JSON.parse(
-  //     localStorage.getItem("appointment-details") || "[]"
-  //   );
+  const apps: IAppointment[] = JSON.parse(
+    localStorage.getItem("appointment-details") || "[]"
+  );
 
   return (
     <>
       <h1>Clinic Appointment System</h1>
-      <CreateAppointment />
-      {/* <ManageAppointments /> */}
-      {/* <Dashboard appointments={apps} /> */}
+      <Routes>
+        <Route path="/" element={<CreateAppointment />} />
+        <Route path="/manage" element={<ManageAppointments />} />
+        <Route path="/dashboard" element={<Dashboard appointments={apps} />} />
+      </Routes>
     </>
   );
 }
