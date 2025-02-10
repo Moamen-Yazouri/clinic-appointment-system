dbc0ac9cc3cdc7e26bfac51ec46dfbec4d692018
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-02-11 00:08:19 +0200

    Fix Conflict.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './Providers/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <AuthProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </AuthProvider>
 )
