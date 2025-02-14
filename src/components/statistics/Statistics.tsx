a9107caf38aab4ad891bc83956ecdaa1fef1d42d
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-02-14 10:15:07 +0200

    improve dashboard style using ant design

--- src/components/statistics/Statistics.tsx
+++ src/components/statistics/Statistics.tsx
@@ -1,50 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { IAppointment } from '../../types/types';
-import dayjs from 'dayjs';
+import React, { useEffect, useState } from "react";
+import { Card, Statistic, Row, Col } from "antd";
+import { IAppointment } from "../../types/types";
+import dayjs from "dayjs";
+import "./Statistics.css";
+
 interface IProps {
-    appointments: IAppointment[];
+  appointments: IAppointment[];
 }
 
 const Statistics = (props: IProps) => {
-    const todayDate = new Date().toISOString();
-    const formattedDate = dayjs(todayDate).format("YYYY-MM-DD");
-        const [todayAppointments, setTodayAppointments] = useState<IAppointment[]>([]);
-        const [pendingAppointments, setPendingAppointments] = useState<IAppointment[]>([]);
-        const [confirmedAppointments, setConfirmedAppointments] = useState<IAppointment[]>([]);
-        useEffect(() => {
-            if(props.appointments.length > 0) {
-                const todayApps: IAppointment[] = props.appointments.filter(
-                    (app) => dayjs(app.dateTime).format("YYYY-MM-DD") === formattedDate
-                );
-                const pendingApps = todayApps.filter((app) => app.status.toString() === "Pending");
-                const ConfirmedApps = todayAppointments.filter((app) => app.status.toString() === "Confirmed");
-                setTodayAppointments(todayApps);
-                setPendingAppointments(pendingApps);
-                setConfirmedAppointments(ConfirmedApps);
-            }
-        }, [props.appointments])
-    return (
-        <div className="statistics">
-            <div className="card todayApp">
-                Total Appointments Today:
-                <span>
-                    {todayAppointments.length}
-                </span> 
-            </div>
-            <div className="card pending">
-                Pending Appointments: 
-                <span>
-                    {pendingAppointments.length}
-                </span> 
-            </div>
-            <div className="card confirmed">
-                Confirmed Appointments: 
-                <span>
-                    {confirmedAppointments.length}
-                </span> 
-            </div>
-        </div>
-    )
-}
+  const todayDate = new Date().toISOString();
+  const formattedDate = dayjs(todayDate).format("YYYY-MM-DD");
+
+  const [todayAppointments, setTodayAppointments] = useState<IAppointment[]>([]);
+  const [pendingAppointments, setPendingAppointments] = useState<IAppointment[]>([]);
+  const [confirmedAppointments, setConfirmedAppointments] = useState<IAppointment[]>([]);
+
+  useEffect(() => {
+    if (props.appointments.length > 0) {
+      const todayApps: IAppointment[] = props.appointments.filter(
+        (app) => dayjs(app.dateTime).format("YYYY-MM-DD") === formattedDate
+      );
+      const pendingApps = todayApps.filter((app) => app.status.toString() === "Pending");
+      const confirmedApps = todayApps.filter((app) => app.status.toString() === "Confirmed");
+
+      setTodayAppointments(todayApps);
+      setPendingAppointments(pendingApps);
+      setConfirmedAppointments(confirmedApps);
+    }
+  }, [props.appointments]);
+
+  return (
+    <Row gutter={[16, 16]}>
+      <Col span={8}>
+        <Card className="first-card">
+          <Statistic title="Today Appointments" value={todayAppointments.length} />
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card className="second-card">
+          <Statistic title="Pending Appointments" value={pendingAppointments.length} />
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card className="third-card">
+          <Statistic title="Confirmed Appointments"  valueStyle={{color: "white"}} value={confirmedAppointments.length}/>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
 
 export default Statistics;
