43258858c4b32e1a42858ea8b712c4ce745120de
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2025-02-14 14:50:53 +0200

    Refactor state placement and fix update issue

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,29 @@ import ManageAppointments from "./Screens/ManageAppointments/ManageAppointments"
 import Dashboard from "./Screens/Dashboard/Dashboard";
 import { IAppointment } from "./types/types";
 import Header from "./components/header/Header";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./Providers/AuthContext";
 import Error404 from "./Screens/Error404/Error404";
+import dayjs from "dayjs";
 
 function App() {
-  const apps: IAppointment[] = JSON.parse(
-    localStorage.getItem("appointment-details") || "[]"
-  );
+  const [appointments, setAppointments] = useState<IAppointment[]>([]);
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    const data = localStorage.getItem("appointment-details");
+    if (data) {
+      let parsedData = JSON.parse(data);
+      if (Array.isArray(parsedData)) {
+        parsedData = [...parsedData].sort(
+          (a, b) => dayjs(a.dateTime).valueOf() - dayjs(b.dateTime).valueOf()
+        );
+        setAppointments(parsedData);
+      }
+    } else {
+      setAppointments([]);
+    }
+  }, []);
 
   return (
     <>
@@ -22,8 +36,19 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/create" element={<CreateAppointment />} />
-        <Route path="/manage" element={<ManageAppointments />} />
-        <Route path="/dashboard" element={<Dashboard appointments={apps} />} />
+        <Route
+          path="/manage"
+          element={
+            <ManageAppointments
+              appointments={appointments}
+              setAppointments={setAppointments}
+            />
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={<Dashboard appointments={appointments} />}
+        />
         <Route path="/*" element={<Error404 />}></Route>
       </Routes>
     </>
--- src/Screens/Dashboard/Dashboard.tsx
+++ src/Screens/Dashboard/Dashboard.tsx
@@ -1,24 +1,23 @@
 import "./Dashboard.css";
-import { IAppointment} from "../../types/types";
+import { IAppointment } from "../../types/types";
 import Statistics from "../../components/statistics/Statistics";
 import Chart from "../../components/chart/Chart";
 import { Typography } from "antd";
 interface IProps {
-    appointments: IAppointment[];
+  appointments: IAppointment[];
 }
 const { Title } = Typography;
 const Dashboard = (props: IProps) => {
-    return (
-        <div className="dashboard">
-            <Title className="main-header" level={1} style={{ color: "#646cff" }}>
-                Dashboard
-            </Title>
-            <Statistics appointments={props.appointments}/>
-            {
-                props.appointments.length > 0 && <Chart appointments={props.appointments} />
-            }
-        </div>
-        
-    )
-}
+  return (
+    <div className="dashboard">
+      <Title className="main-header" level={1} style={{ color: "#646cff" }}>
+        Dashboard
+      </Title>
+      <Statistics appointments={props.appointments} />
+      {props.appointments.length > 0 && (
+        <Chart appointments={props.appointments} />
+      )}
+    </div>
+  );
+};
 export default Dashboard;
