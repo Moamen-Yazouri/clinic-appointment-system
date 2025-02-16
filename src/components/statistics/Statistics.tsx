38434b4c23601f1048733bdcd94814e1b5a5dde0
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-02-16 21:15:33 +0200

    Some Styling fixing...

--- src/components/statistics/Statistics.tsx
+++ src/components/statistics/Statistics.tsx
@@ -43,8 +43,8 @@ const Statistics = (props: IProps) => {
   }, [props.appointments]);
 
   return (
-    <Row  className="cards-wrapper">
-      <Col xs={24} sm={12} md={8} lg={6}>
+    <Row  gutter={[16, 16]} justify="center" className="cards-wrapper">
+      <Col xs={24} sm={12} md={8} lg={8}>
         <Card className="first-card">
           <Statistic
             title="Today Appointments"
@@ -52,7 +52,7 @@ const Statistics = (props: IProps) => {
           />
         </Card>
       </Col>
-      <Col xs={24} sm={12} md={8} lg={6}>
+      <Col xs={24} sm={12} md={8} lg={8}>
         <Card className="second-card">
           <Statistic
             title="Pending Appointments"
@@ -60,7 +60,7 @@ const Statistics = (props: IProps) => {
           />
         </Card>
       </Col>
-      <Col xs={24} sm={12} md={8} lg={6}>
+      <Col xs={24} sm={12} md={8} lg={8}>
         <Card className="third-card">
           <Statistic
             title="Confirmed Appointments"
