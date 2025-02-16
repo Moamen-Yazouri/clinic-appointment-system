a7579dd1b4693825d4885ce175521dc9478e1d55
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-02-16 13:26:50 +0200

    Edit some general styles...

--- src/Providers/AuthContext.tsx
+++ src/Providers/AuthContext.tsx
@@ -5,21 +5,26 @@ interface IAuthContext {
   login: (data: ILoginData) => void;
   logout: () => void;
   user: ILoginData | null;
+  isNaved: boolean
+  setIsNaved: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 interface IProps {
   children: React.ReactNode;
 }
 export const AuthContext = createContext<IAuthContext>({
-  login: () => {},
-  logout: () => {},
+  login: () => { },
+  logout: () => { },
   user: null,
+  isNaved: false,
+  setIsNaved: () => { }
 });
 export const AuthProvider = ({ children }: IProps) => {
   const [user, setUser] = useState<ILoginData | null>(() => {
     const storedUser = localStorage.getItem("user-data");
     return storedUser ? JSON.parse(storedUser) : null;
   });
+  const [isNaved, setIsNaved] = useState(false)
 
   const login = (data: ILoginData) => {
     setUser(data);
@@ -27,8 +32,9 @@ export const AuthProvider = ({ children }: IProps) => {
   };
   const logout = () => {
     setUser(null);
+    setIsNaved(false);
     localStorage.removeItem("user-data");
   };
-  const value = { user, login, logout };
+  const value = { user, login, logout, isNaved, setIsNaved };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
